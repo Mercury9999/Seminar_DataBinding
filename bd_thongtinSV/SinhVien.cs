b062532b6440b259d920e028704f488a6534508f
Pham Hung <hungpham3050@gmail.com>
2024-10-30 22:39:29 +0700

    upload commit

---
+++ bd_thongtinSV/SinhVien.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bd_thongtinSV
+{
+    class SinhVien
+    {
+        private decimal _HocBong;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string HoTen { get; set; }
+        public string ID { get; set; }
+
+        public string Lop { get; set; }
+        public DateTime NgayNhapHoc { get; set; }
+        public decimal HocBong
+        {
+            get => _HocBong;
+            set
+            {
+                _HocBong = value;
+
+                // Support TwoWay binding
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HocBong)));
+            }
+        }
+
+    }
+}
