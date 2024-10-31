edcc32f23329e794c75179f474e38f3e72284b17
Pham Hung <hungpham3050@gmail.com>
2024-10-31 16:19:29 +0700

    new adjustment

--- SampleBinding/MainWindow.xaml.cs
+++ SampleBinding/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace bd_thongtinSV
+namespace SampleBinding
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
